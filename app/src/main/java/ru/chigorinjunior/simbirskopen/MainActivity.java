220d16b977d04798b08f647b9147ca7be1f67d85
Денис Макаров < >
2016-08-27 22:48:17 +0400

    Implemented restoring fragments by tag

--- app/src/main/java/ru/chigorinjunior/simbirskopen/MainActivity.java
+++ app/src/main/java/ru/chigorinjunior/simbirskopen/MainActivity.java
@@ -13,7 +13,9 @@ import ru.chigorinjunior.simbirskopen.ui.results.ResultsFragment;
 import ru.chigorinjunior.simbirskopen.ui.schedule.ScheduleFragment;
 
 public class MainActivity extends AppCompatActivity {
-    BottomBar mBottomBar;
+    private static final String SCHEDULE_FRAGMENT_TAG = "ScheduleFragment";
+    private static final String RESULTS_FRAGMENT_TAG = "ResultsFragment";
+
     Fragment mFragment;
 
     @Override
@@ -25,30 +27,15 @@ public class MainActivity extends AppCompatActivity {
         initBottomBar(savedInstanceState);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        Fragment fragment = new ScheduleFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, fragment).commit();
-    }
-
     private void initBottomBar(Bundle savedInstanceState) {
-        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        BottomBar mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItems(R.menu.main_bottom_bar);
         mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
             @Override
             public void onMenuTabSelected(@IdRes int menuItemId) {
-                switch (menuItemId) {
-                    case R.id.bottom_bar_schedule_item:
-                        mFragment = new ScheduleFragment();
-                        break;
-                    case R.id.bottom_bar_results_item:
-                        mFragment = new ResultsFragment();
-                        break;
-                }
-
-                openScreen();
+                String fragmentTag = getFragmentTag(menuItemId);
+                mFragment = getSupportFragmentManager().findFragmentByTag(fragmentTag);
+                openScreen(fragmentTag);
             }
 
             @Override
@@ -58,7 +45,33 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void openScreen() {
-        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, mFragment).commit();
+    private void openScreen(String fragmentTag) {
+        if (mFragment == null) {
+            mFragment = getFragmentByTag(fragmentTag);
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main, mFragment).commit();
+    }
+
+    private String getFragmentTag(@IdRes int menuItemId) {
+        switch (menuItemId) {
+            case R.id.bottom_bar_schedule_item:
+                return SCHEDULE_FRAGMENT_TAG;
+            case R.id.bottom_bar_results_item:
+                return RESULTS_FRAGMENT_TAG;
+            default:
+                return SCHEDULE_FRAGMENT_TAG;
+        }
+    }
+
+    private Fragment getFragmentByTag(String fragmentTag) {
+        switch (fragmentTag) {
+            case SCHEDULE_FRAGMENT_TAG:
+                return new ScheduleFragment();
+            case RESULTS_FRAGMENT_TAG:
+                return new ResultsFragment();
+            default:
+                return new ScheduleFragment();
+        }
     }
 }
