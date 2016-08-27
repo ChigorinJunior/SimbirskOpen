a2fbe6a0f5eed54bbd27915be94d7920ffc61a77
Денис Макаров < >
2016-08-27 21:52:54 +0400

    Added bottom navigation bar

--- app/src/main/java/ru/chigorinjunior/simbirskopen/MainActivity.java
+++ app/src/main/java/ru/chigorinjunior/simbirskopen/MainActivity.java
@@ -1,17 +1,17 @@
 package ru.chigorinjunior.simbirskopen;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.widget.FrameLayout;
 
 import com.firebase.client.Firebase;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import ru.chigorinjunior.simbirskopen.domain.Team;
 import ru.chigorinjunior.simbirskopen.ui.results.ResultsAdapter;
 import ru.chigorinjunior.simbirskopen.ui.schedule.ScheduleFragment;
 
@@ -26,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
     Firebase mFirebase;
 
+    BottomBar mBottomBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        initBottomBar(savedInstanceState);
     }
 
     @Override
@@ -48,5 +51,33 @@ public class MainActivity extends AppCompatActivity {
 //        mFirebase = new Firebase("https://simbirskopen-7074c.firebaseio.com/results");
 //        mResultsAdapter = new ResultsAdapter(Team.class, R.layout.result_list_item, ResultsAdapter.ResultsViewHolder.class, mFirebase);
 //        mTeamsResults.setAdapter(mResultsAdapter);
+    }
+
+    private void initBottomBar(Bundle savedInstanceState) {
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.main_bottom_bar);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+//                switch (menuItemId) {
+//                    case R.id.bottom_bar_schedule_item:
+//                        mFragmentType = FragmentType.MAP;
+//                        break;
+//                    case R.id.bottom_bar_map_item:
+//                        mFragmentType = FragmentType.MAP;
+//                        break;
+//                    default:
+//                        mFragmentType = FragmentType.MAP;
+//                        break;
+//                }
+//
+//                openScreen();
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                // Do nothing
+            }
+        });
     }
 }
